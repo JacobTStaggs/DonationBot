eeefafc8d47f2090685a6ce762146520b2ca9694
Jacob Staggs <staggs11@marshall.edu>
2017-05-24 15:38:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,7 +45,7 @@ const editables = {
      adminID: ['765611981214343220'], //AdminID, will be used to accept/send messages from/to adminID can have multiple admins Owner needs to be in both
     ownerID: '76561198121434322', //Decided to be lazy and do it this way, this will make sure you receive updates on the bot only 1 owner
     botName: "Shady's bot", //What you want your bot name to show as on steam-user
-    game: 'Twitch.tv/ShadyDaDev', //What game you are playing
+    game: 'Whatever game', //What game you are playing
   },
   Messages:{
     donation: 'Thank you for the donation!', //Message sent to user when a donation is received.
